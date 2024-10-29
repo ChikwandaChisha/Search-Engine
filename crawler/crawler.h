2d60587032afb7b77897541fb4534a36f8fedc73
Chikwanda Chisha <f006fv4@plank.thayer.dartmouth.edu>
2024-10-29 02:52:34 -0400

    added crawler.h

---
+++ crawler/crawler.h
@@ -0,0 +1,33 @@
+/* crawler.h - header file for CS50 Tiny Search Engine Crawler
+ *
+ * This file contains function declarations, constants, and any necessary
+ * types needed by the Crawler module for CS50 TSE. The Crawler starts
+ * from a seed URL and explores all reachable pages up to a specified
+ * depth, saving each page's content in a designated directory.
+ *
+ * Author: Chikwanda Chisha
+ * Date: 18 October, 2024.
+ */
+
+
+#ifndef __CRAWLER_H
+#define __CRAWLER_H
+
+#include "webpage.h"
+
+/* Maximum depth for crawling */
+#define MAX_DEPTH 10
+
+/* Parse command-line arguments, validate input, normalize seed URL */
+void parseArgs(const int argc, char* argv[], char** seedURL, char** pageDirectory, int* maxDepth);
+
+/* Start crawling from seedURL to maxDepth, saving pages to pageDirectory */
+void crawl(char* seedURL, char* pageDirectory, const int maxDepth);
+
+/* Scan webpage for URLs, adding new, internal ones to pagesToCrawl and pagesSeen */
+void pageScan(webpage_t* page, bag_t* pagesToCrawl, hashtable_t* pagesSeen);
+
+/* Free function for URLs in the hashtable */
+void delete_url(void* item);
+
+#endif // __CRAWLER_H
