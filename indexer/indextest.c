5ef40f66dae4700fa1741ae73f038c2f020819a2
Chikwanda Chisha <f006fv4@plank.thayer.dartmouth.edu>
2024-11-02 14:27:55 -0400

    corrected erros in indextest.c

--- indexer/indextest.c
+++ indexer/indextest.c
@@ -1,61 +1,50 @@
 /*
- * indextest.c - Tiny Search Engine Index Test
+ * indextest.c - test program for TSE indexer module
  *
- * This program loads an index file produced by the indexer, saves it to a new file, 
- * and verifies that it can be read and written in the expected format.
+ * Usage: ./indextest oldIndexFilename newIndexFilename
+ * 
+ * This program loads an index file, saves it to a new file,
+ * and ensures the contents match.
  *
  * Author: Chikwanda Chisha
- * Date: 28 October, 2024.
+ * Date: October 29, 2024
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "../common/index.h"
-#include "../common/pagedir.h"
+#include <string.h>
+#include "../libcs50/mem.h"
 #include "../libcs50/file.h"
+#include "../libcs50/hashtable.h"
+#include "../libcs50/counters.h"
+#include "../common/index.h"
 
-static void test(const char* oldIndexFilename, const char* newIndexFilename);
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) 
+{
+    // Check command line arguments
     if (argc != 3) {
-        fprintf(stderr, "usage: ./indextest oldIndexFilename newIndexFilename\n");
-        exit(1);
+        fprintf(stderr, "Usage: %s oldIndexFilename newIndexFilename\n", argv[0]);
+        return 1;
     }
 
-    const char* oldIndexFilename = argv[1];
-    const char* newIndexFilename = argv[2];
+    char *oldIndexFilename = argv[1];
+    char *newIndexFilename = argv[2];
 
-    test(oldIndexFilename, newIndexFilename);
-
-    exit(0);
-}
-
-/**************** test() ****************/
-/* Loads an index from oldIndexFilename, writes it to newIndexFilename, 
- * and deletes the in-memory index after testing.
- */
-static void test(const char* oldIndexFilename, const char* newIndexFilename) {
-    // Initialize a new index
-    index_t* index = index_new();
+    // Load the old index
+    index_t *index = index_load(oldIndexFilename);
     if (index == NULL) {
-        fprintf(stderr, "Error: could not initialize index\n");
-        exit(2);
+        fprintf(stderr, "Error: Could not load index from %s\n", oldIndexFilename);
+        return 1;
     }
 
-    // Load the index data from the old index file
-    if (!index_load(oldIndexFilename)) { // Pass filename directly
-        fprintf(stderr, "Error: could not load index from %s\n", oldIndexFilename);
+    // Save to the new file
+    if (!index_save(index, newIndexFilename)) {
+        fprintf(stderr, "Error: Could not save index to %s\n", newIndexFilename);
         index_delete(index);
-        exit(3);
+        return 1;
     }
 
-    // Write the loaded index to the new index file
-    if (!index_save(index, newIndexFilename)) { // Use index_save instead of index_writeToFile
-        fprintf(stderr, "Error: could not write index to %s\n", newIndexFilename);
-        index_delete(index);
-        exit(5);
-    }
-
-    // Clean up and delete the in-memory index
+    // Clean up
     index_delete(index);
+    return 0;
 }
